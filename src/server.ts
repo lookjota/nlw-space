217f0a7c162505db2743ef28c63221fed0026983
krasny <lookjota@gmail.com>
2023-05-30 17:01:10 -0300

    firt commit

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client'
+import fastify from 'fastify'
+
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+
+  return users
+})
+
+app
+  .listen({
+    port: 4444,
+  })
+  .then(() => {
+    console.log('HTTP server running on http://localhost:4444')
+  })
